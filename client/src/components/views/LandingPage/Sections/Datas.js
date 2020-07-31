b9c8f38749274587ff26ca9b634434ae6a16334c
pramithvidusara@gmail.com <43032202+TilangaPramith@users.noreply.github.com>
2020-07-31 15:13:03 +0530

    UIs

---
+++ client/src/components/views/LandingPage/Sections/Datas.js
@@ -0,0 +1,72 @@
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $19",
+        "array": [0, 19]
+    },
+    {
+        "_id": 2,
+        "name": "$20 to $39",
+        "array": [20, 39]
+    },
+    {
+        "_id": 3,
+        "name": "$40 to $59",
+        "array": [40, 59]
+    },
+    {
+        "_id": 4,
+        "name": "$60 to $79",
+        "array": [60, 79]
+    },
+    {
+        "_id": 5,
+        "name": "More than $80",
+        "array": [80, 1500000]
+    }
+]
+
+
+
+const continents = [
+    {
+        "_id": 1,
+        "name": "Africa"
+    },
+    {
+        "_id": 2,
+        "name": "Europe"
+    },
+    {
+        "_id": 3,
+        "name": "Asia"
+    },
+    {
+        "_id": 4,
+        "name": "North America"
+    },
+    {
+        "_id": 5,
+        "name": "South America"
+    },
+    {
+        "_id": 6,
+        "name": "Australia"
+    },
+    {
+        "_id": 7,
+        "name": "Antarctica"
+    }
+]
+
+
+export {
+    price,
+    continents
+}
